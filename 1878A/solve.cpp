c2fe2d215ff2c9696ae305a40f103c8ce5e12806
MDAlviano <mdalviano.21122@gmail.com>
2026-07-23 21:38:09 +0700

    2

---
+++ 1878A/solve.cpp
@@ -0,0 +1,25 @@
+//acc
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    
+    int t; cin >> t;
+
+    while (t--) {
+        int n, k; cin >> n >> k;
+
+        bool ans = false;
+
+        for (int i = 0; i < n; i++) {
+            int x; cin >> x;
+
+            if (x == k) ans = true;
+        }
+
+        (ans) ? cout << "YES\n" : cout << "NO\n";
+    }
+    
+    return 0;
+}
---
+++ 432A/solve.cpp
@@ -0,0 +1,19 @@
+//acc
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    
+    int n, k, c = 0; cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+
+        if (x + k <= 5) c++;
+    }
+
+    cout << c / 3;
+    
+    return 0;
+}
